6538e0ff59654b918ed0850e58392524d170304a
headcha <headcha@naver.com>
2017-07-13 23:04:59 +0900

    spring boot 전환중

--- core/src/main/java/com/cha/kim/wedding/core/config/CoreApplicationConfig.java
+++ core/src/main/java/com/cha/kim/wedding/core/config/CoreApplicationConfig.java
@@ -2,19 +2,13 @@ package com.cha.kim.wedding.core.config;
 
 import com.cha.kim.wedding.core.CoreApplication;
 import com.cha.kim.wedding.core.util.FullBeanNameGenerator;
-import com.google.common.cache.CacheBuilder;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-
-import java.util.concurrent.TimeUnit;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan(basePackageClasses = CoreApplication.class , nameGenerator = FullBeanNameGenerator.class)
-@PropertySource( value = "classpath:properties/${env}.core.properties" )
-@Import({CoreDataBaseConfig.class})
+@EnableJpaRepositories(basePackageClasses = CoreApplication.class)
 public class CoreApplicationConfig {
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
